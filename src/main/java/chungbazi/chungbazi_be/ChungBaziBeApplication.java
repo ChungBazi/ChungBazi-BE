c8f15ed523fa7b670fdb11f357313b5407fe4709
이예지 <oyatpeach@naver.com>
2025-01-07 18:27:56 +0900

    Initial commit

---
+++ src/main/java/chungbazi/chungbazi_be/ChungBaziBeApplication.java
@@ -0,0 +1,13 @@
+package chungbazi.chungbazi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChungBaziBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChungBaziBeApplication.class, args);
+	}
+
+}
