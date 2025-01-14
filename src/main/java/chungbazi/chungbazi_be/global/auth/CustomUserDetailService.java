2bd0af3aa4db299553f21090c8eee960533b0931
hhongyeahh <hhongyeahh@gmail.com>
2025-01-14 15:44:24 +0900

    [move] SecurityConfig 위치를 global/config로 이동, CustomUserDetailService 위치를 global/auth로 이동

--- src/main/java/chungbazi/chungbazi_be/global/auth/CustomUserDetailService.java
+++ src/main/java/chungbazi/chungbazi_be/global/auth/CustomUserDetailService.java
@@ -1,4 +1,4 @@
-package chungbazi.chungbazi_be.config;
+package chungbazi.chungbazi_be.global.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
