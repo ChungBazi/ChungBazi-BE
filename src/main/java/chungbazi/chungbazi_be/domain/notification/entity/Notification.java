8ac382b66baa80428a90f0b1d3a1b1aa4991305e
dldusgh318 <pololydotoly@gmail.com>
2025-01-21 16:20:51 +0900

    [feature] Notification entity, repository 구현 (#23)

---
+++ src/main/java/chungbazi/chungbazi_be/domain/notification/entity/Notification.java
@@ -0,0 +1,39 @@
+package chungbazi.chungbazi_be.domain.notification.entity;
+
+import chungbazi.chungbazi_be.domain.notification.entity.enums.NotificationType;
+import chungbazi.chungbazi_be.domain.user.entity.User;
+import chungbazi.chungbazi_be.global.entity.BaseTimeEntity;
+import com.google.firebase.database.annotations.NotNull;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private boolean isRead;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private NotificationType type;
+
+    @Column(length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //댓글이랑 연관관계
+
+    //장바구니랑 연관관계
+
+}
