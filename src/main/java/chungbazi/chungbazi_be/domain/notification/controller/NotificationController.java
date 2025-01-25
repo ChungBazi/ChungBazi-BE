cc03e252c5e9c291ad8758b41b3c1b0f13f100fe
dldusgh318 <pololydotoly@gmail.com>
2025-01-25 18:20:34 +0900

    [feat] 알람 생성 controller 로직 구현 (#20)

---
+++ src/main/java/chungbazi/chungbazi_be/domain/notification/controller/NotificationController.java
@@ -0,0 +1,25 @@
+package chungbazi.chungbazi_be.domain.notification.controller;
+
+import chungbazi.chungbazi_be.domain.notification.dto.NotificationRequestDTO;
+import chungbazi.chungbazi_be.domain.notification.dto.NotificationResponseDTO;
+import chungbazi.chungbazi_be.domain.notification.service.NotificationService;
+import chungbazi.chungbazi_be.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notifications")
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @PostMapping
+    @Operation(summary = "알림 생성 API",description = "알림을 생성하고, fcm으로 알림을 요청하는 API입니다.")
+    public ApiResponse<NotificationResponseDTO.responseDto> createNotification(@RequestBody NotificationRequestDTO.createDTO dto) {
+        NotificationResponseDTO.responseDto responseDto = notificationService.sendNotification(dto);
+        return ApiResponse.onSuccess(responseDto);
+    }
+
+
+}
