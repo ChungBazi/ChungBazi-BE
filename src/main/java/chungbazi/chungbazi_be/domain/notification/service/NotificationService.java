01dec0917972fab69dcb1203efc2a170f101c938
dldusgh318 <pololydotoly@gmail.com>
2025-01-28 22:49:13 +0900

    [fix] service 수정 (#33)

--- src/main/java/chungbazi/chungbazi_be/domain/notification/service/NotificationService.java
+++ src/main/java/chungbazi/chungbazi_be/domain/notification/service/NotificationService.java
@@ -14,6 +14,7 @@ import chungbazi.chungbazi_be.global.apiPayload.exception.handler.NotFoundHandle
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class NotificationService {
     private final UserRepository userRepository;
     private final NotificationRepository notificationRepository;
