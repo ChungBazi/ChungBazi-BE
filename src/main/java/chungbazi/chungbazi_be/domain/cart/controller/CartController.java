5127a6fe6c70b2fb49dc921f5d2e1aeeac661018
sh0311 <0311alice@naver.com>
2025-01-28 16:44:22 +0900

    [chore] : develop 머지 23

--- src/main/java/chungbazi/chungbazi_be/domain/cart/controller/CartController.java
+++ src/main/java/chungbazi/chungbazi_be/domain/cart/controller/CartController.java
@@ -1,7 +1,9 @@
 package chungbazi.chungbazi_be.domain.cart.controller;
 
 import chungbazi.chungbazi_be.domain.cart.service.CartService;
+import chungbazi.chungbazi_be.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,7 @@ public class CartController {
 
     // 장바구니에 담기
     @PostMapping("/{policyId}")
-    public ApiResponse
+    public ApiResponse<String> addPolicyToCart(@PathVariable("policyId") String policyId) {
+        
+    }
 }
