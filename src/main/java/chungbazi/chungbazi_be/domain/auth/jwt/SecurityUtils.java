eff6cec35e61973a3725c1553b73610990c1c98b
이예지 <oyatpeach@naver.com>
2025-01-28 18:17:43 +0900

    [chore] error 처리 #36

--- src/main/java/chungbazi/chungbazi_be/domain/auth/jwt/SecurityUtils.java
+++ src/main/java/chungbazi/chungbazi_be/domain/auth/jwt/SecurityUtils.java
@@ -1,11 +1,13 @@
 package chungbazi.chungbazi_be.domain.auth.jwt;
+import chungbazi.chungbazi_be.global.apiPayload.code.status.ErrorStatus;
+import chungbazi.chungbazi_be.global.apiPayload.exception.handler.NotFoundHandler;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 public class SecurityUtils {
     public static Long getUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) {
-            throw new IllegalStateException("not found");
+            throw new NotFoundHandler(ErrorStatus.NOT_FOUND_USER);
         }
         return Long.valueOf(authentication.getName());
         }
