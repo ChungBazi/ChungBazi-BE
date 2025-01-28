15f6148abc7b792c49cd7f08907191cbe55040bb
dldusgh318 <pololydotoly@gmail.com>
2025-01-28 22:44:05 +0900

    [feat] 알림 무한 스크롤 페이징 로직 (#33)

--- src/main/java/chungbazi/chungbazi_be/domain/notification/repository/NotificationRepositoryImpl.java
+++ src/main/java/chungbazi/chungbazi_be/domain/notification/repository/NotificationRepositoryImpl.java
@@ -7,19 +7,24 @@ import com.google.api.gax.paging.Page;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class NotificationRepositoryImpl implements NotificationRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
-    private final QNotification qNotification=QNotification.notification;
 
 
-        //알림 읽음 처리
+    //알림 읽음 처리
     @Override
     public void markAllAsRead(Long userId, NotificationType type){
+
+        QNotification qNotification=QNotification.notification;
+
         BooleanBuilder booleanBuilder = new BooleanBuilder();
         booleanBuilder.and(qNotification.user.id.eq(userId));
         booleanBuilder.and(qNotification.isRead.eq(false));
@@ -33,4 +38,34 @@ public class NotificationRepositoryImpl implements NotificationRepositoryCustom{
                 .where(booleanBuilder)
                 .execute();
     }
+
+    //알림 조회
+    @Override
+    public List<Notification> findNotificationsByUserIdAndNotificationType(Long userId, NotificationType type, Long cursor, int limit) {
+
+        QNotification qNotification=QNotification.notification;
+
+        BooleanBuilder booleanBuilder=new BooleanBuilder();
+        booleanBuilder.and(qNotification.user.id.eq(userId));
+
+        //타입필터
+        if(type!=null){
+            booleanBuilder.and(qNotification.type.eq(type));
+        }
+        if(cursor!=null){
+            booleanBuilder.and(qNotification.id.lt(cursor));
+        }
+
+        //페이징 조회
+        List<Notification> notifications=queryFactory
+                .selectFrom(qNotification)
+                .where(booleanBuilder)
+                .orderBy(qNotification.createdAt.desc())
+                .limit(limit)
+                .fetch();
+
+        return notifications;
+    }
+
+
 }
