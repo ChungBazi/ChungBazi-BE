269c6ce4d9e186ba1d32e786ae55efbfca7d4490
hhongyeahh <hhongyeahh@gmail.com>
2025-02-14 21:37:18 +0900

    [feat] 회원탈퇴 익명화

--- src/main/java/chungbazi/chungbazi_be/domain/user/repository/UserRepository.java
+++ src/main/java/chungbazi/chungbazi_be/domain/user/repository/UserRepository.java
@@ -2,7 +2,11 @@ package chungbazi.chungbazi_be.domain.user.repository;
 
 import chungbazi.chungbazi_be.domain.community.entity.Post;
 import chungbazi.chungbazi_be.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,5 +15,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.name = '알 수 없음', u.email = CONCAT('deleted_', u.id, '@yourdomain.com') WHERE u.id = :userId")
+    void anonymizeUser(@Param("userId") Long userId);
 
 }
