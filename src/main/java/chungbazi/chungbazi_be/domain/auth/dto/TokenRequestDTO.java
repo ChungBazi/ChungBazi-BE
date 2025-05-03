041b91d7e832e5567948c6e25d26b2ca580fb4cc
hhongyeahh <hhongyeahh@gmail.com>
2025-05-03 16:46:55 +0900

    [feat] 일반 로그인 및 회원가입 api #64

--- src/main/java/chungbazi/chungbazi_be/domain/auth/dto/TokenRequestDTO.java
+++ src/main/java/chungbazi/chungbazi_be/domain/auth/dto/TokenRequestDTO.java
@@ -16,7 +16,59 @@ public class TokenRequestDTO {
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
+    public static class SignUpTokenRequestDTO {
+
+        @NotBlank(message = "이름은 필수 입력 항목입니다.")
+        @Size(min = 1, max = 10, message = "이름은 10자 이하")
+        @Schema(example = "김바지", description = "사용자의 이름")
+        private String name;
+
+        @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+        @Pattern(
+                regexp = RegexConstants.EMAIL_REGEX,
+                message = "유효한 이메일 주소여야 합니다."
+        )
+        @Schema(example = "chungbazi@example.com", description = "유효한 이메일 주소")
+        private String email;
+
+        @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+        @Schema(example = "password1234!", description = "사용자의 비밀번호")
+        private String password;
+
+        @NotBlank(message = "비밀번호 확인은 필수 입력 항목입니다.")
+        @Schema(example = "password1234!", description = "사용자의 비밀번호")
+        private String checkPassword;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class LoginTokenRequestDTO {
+
+        @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+        @Pattern(
+                regexp = RegexConstants.EMAIL_REGEX,
+                message = "유효한 이메일 주소여야 합니다."
+        )
+        @Schema(example = "chungbazi@example.com", description = "유효한 이메일 주소")
+        private String email;
+
+        @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+        @Schema(example = "password1234!", description = "사용자의 비밀번호")
+        private String password;
+
+        @Schema(example = "eKD_mr9SG0XftaFoKFB9L5ABA91bF1cpjv7g6Khw...", description = "유효한 FCM Token")
+        private String fcmToken;
+    }
+
+
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SocialLoginTokenRequestDTO {
 
         @NotBlank(message = "이름은 필수 입력 항목입니다.")
         @Size(min = 1, max = 10, message = "이름은 10자 이하")
