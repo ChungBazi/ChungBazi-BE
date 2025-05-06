b853d62e3bf07acbc1f35a21aeef2a9aafef8714
이예지 <oyatpeach@naver.com>
2025-05-06 11:45:58 +0900

    [feat] ExistEntity annotation 생성 #93

---
+++ src/main/java/chungbazi/chungbazi_be/global/validation/annotation/ExistEntity.java
@@ -0,0 +1,20 @@
+package chungbazi.chungbazi_be.global.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+//@Constraint(validatedBy = )
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistEntity {
+    String message() default "";
+    Class<?> entityType();
+    Class<?> [] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
