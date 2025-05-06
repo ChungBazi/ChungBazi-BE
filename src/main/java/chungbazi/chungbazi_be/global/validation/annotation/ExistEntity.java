43e043687ec14ed37264fe6d7b6d39644a87ef3f
이예지 <oyatpeach@naver.com>
2025-05-06 11:57:26 +0900

    [feat] entityExistValidator 생성 #93

--- src/main/java/chungbazi/chungbazi_be/global/validation/annotation/ExistEntity.java
+++ src/main/java/chungbazi/chungbazi_be/global/validation/annotation/ExistEntity.java
@@ -1,5 +1,6 @@
 package chungbazi.chungbazi_be.global.validation.annotation;
 
+import chungbazi.chungbazi_be.global.validation.validator.EntityExistValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
@@ -9,7 +10,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-//@Constraint(validatedBy = )
+@Constraint(validatedBy = EntityExistValidator.class)
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExistEntity {
---
+++ src/main/java/chungbazi/chungbazi_be/global/validation/validator/EntityExistValidator.java
@@ -0,0 +1,48 @@
+package chungbazi.chungbazi_be.global.validation.validator;
+
+import chungbazi.chungbazi_be.global.apiPayload.code.status.ErrorStatus;
+import chungbazi.chungbazi_be.global.validation.annotation.ExistEntity;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EntityExistValidator implements ConstraintValidator<ExistEntity, Long> {
+    private final Map<Class<?>, JpaRepository<?, Long>> repositoryMap;
+    private final Map<Class<?>, ErrorStatus> errorStatusMap;
+
+    private Class<?> entityType;
+
+    @Override
+    public void initialize(ExistEntity constraintAnnotation) {this.entityType = constraintAnnotation.entityType()};
+
+    @Override
+    public boolean isValid(Long value, ConstraintValidatorContext context) {
+        if (value == null) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("id는 필수입니다.").addConstraintViolation();
+            return false;
+        }
+
+        JpaRepository<?, Long> repository = repositoryMap.get(entityType);
+        if (repository == null) {
+            throw new IllegalArgumentException("Repository not found for entity type: " + entityType.getName());
+        }
+
+        boolean exists = repository.existsById(value);
+
+        if (!exists) {
+            context.disableDefaultConstraintViolation();
+
+            //동적 메세지 설정
+            String msg = errorStatusMap.get(entityType).getMessage();
+            context.buildConstraintViolationWithTemplate(msg).addConstraintViolation();
+        }
+
+        return exists;
+    }
+}
