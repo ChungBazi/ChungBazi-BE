60fc870944ff7b9206f7651fc0aa97f9274872b8
이예지 <oyatpeach@naver.com>
2025-06-18 18:17:11 +0900

    [fix] 누락된 어노테이션 추가 #99

--- src/main/java/chungbazi/chungbazi_be/domain/chatbot/dto/ChatBotRequestDTO.java
+++ src/main/java/chungbazi/chungbazi_be/domain/chatbot/dto/ChatBotRequestDTO.java
@@ -3,11 +3,15 @@ package chungbazi.chungbazi_be.domain.chatbot.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class ChatBotRequestDTO {
     @Getter
+    @Setter
     @Builder
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class ChatDto {
         String message;
     }
--- src/main/java/chungbazi/chungbazi_be/global/utils/OpenAiProperties.java
+++ src/main/java/chungbazi/chungbazi_be/global/utils/OpenAiProperties.java
@@ -1,12 +1,14 @@
 package chungbazi.chungbazi_be.global.utils;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "openai")
 @Getter
+@Setter
 public class OpenAiProperties {
     private String apiKey;
     private String model;
