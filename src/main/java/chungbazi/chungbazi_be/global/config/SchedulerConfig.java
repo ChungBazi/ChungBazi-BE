b2473eb28e8be7984293e40df97c1e44ac6f228c
dldusgh318 <pololydotoly@gmail.com>
2025-11-30 23:58:22 +0900

    fix: scheduler 수정 (#156)

--- src/main/java/chungbazi/chungbazi_be/global/config/SchedulerConfig.java
+++ src/main/java/chungbazi/chungbazi_be/global/config/SchedulerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-@Profile("prod")
+//@Profile("prod")
 @Configuration
 public class SchedulerConfig {
 
